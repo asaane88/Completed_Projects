11d0a273c78dcdc30dda6f8d9b780d76ded5b627
Arvind Saane <48819726+asaane88@users.noreply.github.com>
2021-02-03 10:21:44 +0530

    Delete Python_Projects/Banking-System directory

--- Python_Projects/Banking-System/connection.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'user'
-
-import cx_Oracle
-//you must have Oracle database userId and password for connection from database.
-con = cx_Oracle.connect("userId/password@localhost/xe")
-cur = con.cursor()
